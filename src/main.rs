b4aa786cb96c339930bbdd37e67bf815760c7ff0
xavier-kong <kongxianchengxavier@gmail.com>
2023-07-01 13:50:00 +0800

    db connection and query

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use serde::{Deserialize, Serialize};
+use planetscale_driver::{query, Database, PSConnection};
+use std::env::var;
+use anyhow::Result;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+//// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+
 #[derive(Debug, Deserialize)]
 struct MyRequest {
     offset: String,
 }
 
+#[derive(Database, Debug)]
+pub struct Event {
+    id: i32,
+    name: String,
+    timestamp_seconds: i32,
+    headline: String,
+    url: String,
+    org: String
+}
+
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let request_body: MyRequest = serde_json::from_slice(event.body().as_ref())?;
 
-    let offset_int: i8 = match request_body.offset.parse() {
-        Ok(res) => res,
-        Err(_) => 0
-    };
+    let offset_int: i8 = request_body.offset.parse().unwrap_or(0);
 
+    let conn = PSConnection::new_from_env().unwrap();
 
+    let curr_time_secs = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs();
 
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    let query_string = std::format!("SELECT * FROM events WHERE timestamp_seconds > {}", curr_time_secs);
+
+    let query_res: Vec<Event> = query(query_string.as_str()).fetch_all(&conn).await?;
+    let message = "hello";
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
